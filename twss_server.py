5dcacd62670fc02929e267fa85b25cd9706cf6fa
Harshad Sharma <harshad@harshadsharma.com>
2012-04-10 04:29:07 +0530

    Web server works, with a simple form implementation at /

--- twss_server.py
+++ twss_server.py
@@ -1,6 +1,13 @@
 import web
 import json
 import twsslib
+from web import form
+
+render = web.template.render('templates/')
+
+myform = form.Form(
+    form.Textbox('query'),
+)
         
 urls = (
     '/train/(.*)', 'train',
@@ -10,7 +17,11 @@ urls = (
 
 app = web.application(urls, globals())
 twss = twsslib.TextClassifier()
-twss.train()
+try:
+    twss.load()
+except:
+    twss.train()
+    twss.save()
 
 class query:
     def GET(self, text):
@@ -26,8 +37,20 @@ class query:
 
 class index:
     def GET(self):
-        return 'Welcome!'
+        form = myform()
+        return render.inputform(form=form, istwss=None)
 
+    def POST(self):
+        form = myform()
+        if not form.validates():
+            return render.inputform(form)
+        else:
+            try:
+                istwss = twss.is_positive(form.query.value)
+            except:
+                istwss = False
+            
+            return render.inputform(form=form, istwss=istwss)
 
 
 if __name__ == '__main__':
