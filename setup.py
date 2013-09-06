ddadec65a921e417afd546e07684048e01f4d2e4
Aditya Sengupta <aditya@sengupta.me>
2013-09-06 15:10:45 +0530

    Adding a setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup 
+
+setup(name='twss',
+        version='0.1',
+        description='A Naive Bayes classifier that can identify double entendres.',
+        url='https://github.com/sengupta/twss',
+        author='Aditya Sengupta',
+        author_email='aditya@sengupta.me',
+        license='WTFPL',
+        packages=['twss',],
+        zip_safe=False,
+        )
