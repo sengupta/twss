78ed6cae627a34e83424a82a4ff7996087d17f5b
Aditya Sengupta <aditya@sengupta.me>
2013-09-10 19:36:12 +0530

    0.1.5: updated package_dir and package_data

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup 
 
 setup(name='twss',
-        version='0.1.4',
+        version='0.1.5',
         description='A Naive Bayes classifier that can identify double entendres.',
         url='https://github.com/sengupta/twss',
         author='Aditya Sengupta',
         author_email='aditya@sengupta.me',
         license='WTFPL',
         packages=['twss',],
+        package_dir={'twss': 'twss'},
+        package_data={'twss': ['*.txt']},
         install_requires=[
             "nltk==2.0.4",
             ],
